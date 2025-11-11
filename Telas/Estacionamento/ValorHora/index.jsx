db85faf1a4d39603b26b06a5ba82d4d5ec5fb4a8
João Gabriel <jotha77gabriel@gmail.com>
2025-11-11 01:23:27 -0300

    finalmente acabou

--- Telas/Estacionamento/ValorHora/index.jsx
+++ Telas/Estacionamento/ValorHora/index.jsx
@@ -1,8 +1,8 @@
 import { View, Text, Alert } from "react-native";
 import { Button } from "react-native-paper";
 import { firestore } from '../../../firebaseConfig'; 
-import { useState } from 'react';
-import { collection, addDoc } from "firebase/firestore"; 
+import { useState, useEffect } from 'react';
+import { collection, addDoc , doc , setDoc} from "firebase/firestore"; 
 import Estilos from "../../../Componentes/Estilos";
 import TextoInput from "../../../Componentes/TextoInput";
 
@@ -10,21 +10,46 @@ export default function ValorHora(props) {
     const [valorHora, setValorHora] = useState('');
     const [carregando, setCarregando] = useState(false);
 
+    const DOC_ID_VALOR = "precoFixo";
+
+    useEffect(() => {
+        const buscarValorAtual = async () => {
+            const docRef = doc(firestore, "valor", DOC_ID_VALOR);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                setValorHora(docSnap.data().valorHora.toString());
+            }
+        };
+        buscarValorAtual().catch(console.error);
+    }, []);
+
     const DefinirValorHora = async () => {
         if (valorHora === '') {
             Alert.alert("Erro", "Por favor, preencha o valor da hora.");
             return;
         }
 
+        const valorNumerico = parseFloat(valorHora.replace(',', '.')); 
+
+        if (isNaN(valorNumerico) || valorNumerico <= 0) {
+            Alert.alert("Erro", "Por favor, insira um valor numérico válido (Ex: 5.50).");
+            return;
+        }
+
         setCarregando(true);
 
         try {
-            const docRef = await addDoc(collection(firestore, "valor"), {
-                valorHora: parseFloat(valorHora)
-            });
-            console.log("✅ Valor da hora definido com ID: ", docRef.id);
-            Alert.alert("Sucesso", "Valor da hora definido!");
-            setValorHora(''); 
+
+            const docRef = doc(firestore, "valor", DOC_ID_VALOR);
+
+            await setDoc(docRef, { 
+                valorHora: valorNumerico 
+            }, { merge: true });
+
+            console.log("✅ Valor da hora definido: ", valorHora);
+            Alert.alert("Sucesso", "Valor da hora definido com sucesso!");
+            
         } catch (error) {
             console.error("❌ Erro ao definir valor da hora: ", error);
             Alert.alert("Erro", "Não foi possível definir o valor da hora.");
@@ -42,6 +67,7 @@ export default function ValorHora(props) {
                 value={valorHora}
                 setValue={setValorHora}
                 placeholder="Ex: 5.00"
+                keyboardType="numeric"
             />
             
             <Button 
